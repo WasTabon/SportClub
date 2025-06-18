fab7f9ee516269fd6d27b9498c27f70052fa8671
WasTabon <titanium.jen@gmail.com>
2025-06-18 20:22:59 +0300

    ui

--- Assets/SportClub/Scripts/BuffsManager.cs
+++ Assets/SportClub/Scripts/BuffsManager.cs
@@ -1,4 +1,7 @@
+using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public enum BuffType
 {
@@ -8,5 +11,47 @@ public enum BuffType
 
 public class BuffsManager : MonoBehaviour
 {
-   
+    [SerializeField] private RectTransform _content;
+    [SerializeField] private RectTransform _buff;
+
+    [SerializeField] private Sprite _viralIcon;
+
+    public void AddBuff(BuffType buffType)
+    {
+        string description = GetDescriptionByType(buffType);
+        Sprite sprite = GetIconByType(buffType);
+        
+        RectTransform buff = Instantiate(_buff.gameObject, _content).GetComponent<RectTransform>();
+        Image icon = buff.GetComponentInChildren<Image>();
+        TextMeshProUGUI text = buff.GetComponentInChildren<TextMeshProUGUI>();
+
+        icon.sprite = sprite;
+        text.text = description;
+    }
+
+    private string GetDescriptionByType(BuffType buffType)
+    {
+        switch (buffType)
+        {
+            case BuffType.ViralBuff:
+                return "+20% to income";
+            case BuffType.NegativeReactionBuff:
+                return "-10% to loyalty";
+            default:
+                return "";
+        }
+    }
+
+    private Sprite GetIconByType(BuffType buffType)
+    {
+        switch (buffType)
+        {
+            case BuffType.ViralBuff:
+                return _viralIcon;
+            case BuffType.NegativeReactionBuff:
+                return _viralIcon;
+            default:
+                return null;
+        }
+    }
 }
