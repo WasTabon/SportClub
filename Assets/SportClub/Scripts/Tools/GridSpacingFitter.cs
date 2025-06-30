9d8bf3bb8ec79c3c5ccdaae1ca1254820eace53c
WasTabon <titanium.jen@gmail.com>
2025-06-30 12:26:36 +0300

    ui

--- Assets/SportClub/Scripts/Tools/GridSpacingFitter.cs
+++ Assets/SportClub/Scripts/Tools/GridSpacingFitter.cs
@@ -6,51 +6,50 @@ using UnityEngine.UI;
 public class GridSpacingFitter : MonoBehaviour
 {
     public int elementCount = 5;
-    public float elementWidth = 170f;
+    public float elementSize = 80f;
+
+    public float leftPadding = 125f;
+    public float rightPadding = 125f;
+    public float bottomPadding = 55f;
 
     private RectTransform _rectTransform;
     private GridLayoutGroup _grid;
-
-    private bool called;
 
     private void Awake()
     {
         _rectTransform = GetComponent<RectTransform>();
         _grid = GetComponent<GridLayoutGroup>();
+
+        ApplyFitting();
     }
-
-    private void Update()
-    {
-        UpdateSpacing();
-    }
-
-    private void UpdateSpacing()
-    {
-        if (_rectTransform == null || _grid == null) return;
-        if (called) return;
-
-        float totalWidth = _rectTransform.rect.width;
-        float spacing = (totalWidth - elementCount * elementWidth) / (elementCount - 1);
-        spacing = Mathf.Max(spacing, 0);
-
-        _grid.spacing = new Vector2(spacing, _grid.spacing.y);
-        _grid.cellSize = new Vector2(elementWidth, elementWidth);
-
-        called = true;
-    }
-
-#if UNITY_EDITOR
-    private void OnValidate()
-    {
-        if (Application.isPlaying == false && _rectTransform != null && _grid != null)
-        {
-            UpdateSpacing();
-        }
-    }
-#endif
 
     private void OnRectTransformDimensionsChange()
     {
-        UpdateSpacing();
+        ApplyFitting();
+    }
+
+    private void ApplyFitting()
+    {
+        if (_rectTransform == null || _grid == null) return;
+
+        // Safe area Y
+        Rect safeArea = Screen.safeArea;
+        float safeAreaTop = safeArea.yMax;
+        float safeAreaBottom = safeArea.yMin;
+
+        float canvasHeight = _rectTransform.rect.height;
+        float screenToCanvasRatio = canvasHeight / Screen.height;
+
+        float topPadding = (Screen.height - safeAreaTop) * screenToCanvasRatio;
+        float bottom = (safeAreaBottom * screenToCanvasRatio) + bottomPadding;
+
+        // Расчёт доступной ширины с учётом левого и правого отступов
+        float availableWidth = _rectTransform.rect.width - leftPadding - rightPadding;
+        float spacing = (availableWidth - elementCount * elementSize) / (elementCount - 1);
+        spacing = Mathf.Max(spacing, 0);
+
+        _grid.cellSize = new Vector2(elementSize, elementSize);
+        _grid.spacing = new Vector2(spacing, _grid.spacing.y);
+        _grid.padding = new RectOffset((int)leftPadding, (int)rightPadding, (int)topPadding, (int)bottom);
     }
 }
